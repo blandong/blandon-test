690be6688a337f5a6becb8d052bd6f7c53feb19c
Blandon Dong <bai.dong@covisint.com>
2016-04-02 22:10:54 +0800

    java io test

---
+++ src/main/java/com/blandon/test/io/IOTest.java
@@ -0,0 +1,117 @@
+package com.blandon.test.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.commons.io.IOUtils;
+
+import com.google.common.io.CharStreams;
+
+public class IOTest {
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		
+		//bufferedReader();
+		
+		//apacheIOUtils();
+		
+		//apacheIOUtilsToString();
+		
+		readInputStream();
+	}
+	
+	
+	
+	private static void bufferedReader() throws URISyntaxException, IOException{
+		BufferedReader bd = null;
+		try{
+			
+			URI uri = IOTest.class.getClassLoader().getResource("testFile.txt").toURI();
+			
+			File file = new File(uri);
+			
+			InputStream is = new FileInputStream(file);
+			
+			InputStreamReader isr = new InputStreamReader(is);
+			
+			bd = new BufferedReader(isr);	
+			
+			StringBuilder sb = new StringBuilder();
+			
+			String line = null;
+			
+			while((line =bd.readLine()) != null){
+				sb.append(line).append("\n");
+			}
+			
+			System.out.println(sb.toString());
+		}finally{
+			if(bd != null){
+				bd.close();
+			}
+		}
+	}
+	
+	////Method 1 IOUtils.copy()
+	private static void apacheIOUtils() throws FileNotFoundException, IOException, URISyntaxException{
+		
+		URI uri = IOTest.class.getClassLoader().getResource("testFile.txt").toURI();
+		
+		File file = new File(uri);
+		
+        StringWriter writer = new StringWriter();
+        
+        //Method 1 IOUtils.copy()
+        IOUtils.copy(new FileInputStream(file), writer, "UTF-8");
+        
+        String theString = writer.toString();
+        
+        System.out.println(theString);
+       
+	}
+	
+	////Method 2 IOUtils.toString()
+	private static void apacheIOUtilsToString() throws URISyntaxException, FileNotFoundException, IOException{
+		
+		URI uri = IOTest.class.getClassLoader().getResource("testFile.txt").toURI();
+		File file = new File(uri);
+		
+		String theString2 = IOUtils.toString(new FileInputStream(file), "UTF-8");
+		
+        System.out.println(theString2);
+	}
+	
+	//use google guawa
+	private static void readInputStream() throws URISyntaxException, UnsupportedEncodingException, IOException{
+		
+		try{
+			URI uri = IOTest.class.getClassLoader().getResource("testFile.txt").toURI();
+			File file = new File(uri);
+			
+			FileInputStream fis = new FileInputStream(file);
+			
+			String stringFromStream = CharStreams.toString(new InputStreamReader(fis, "UTF-8"));
+			
+			System.out.println(stringFromStream);
+			
+		}finally{
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
