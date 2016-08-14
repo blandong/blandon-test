9f58127d3ccac40f13bbe523502ee5aceb87966b
bdong <bai.dong@covisint.com>
2016-08-14 16:14:32 -0400

    update

--- src/main/java/com/blandon/test/javax/json/JsonTest.java
+++ src/main/java/com/blandon/test/javax/json/JsonTest.java
@@ -1,6 +1,8 @@
 package com.blandon.test.javax.json;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.json.Json;
 import javax.json.JsonArray;
@@ -21,9 +23,13 @@ public class JsonTest {
 	private static final Logger logger = LoggerFactory.getLogger(JsonTest.class);
 	
 	public static void main(String[] args) {
-		Person p = readPersonFromFile();
+		//Person p = readPersonFromFile();
 		
-		jsonWriter(p);
+		//jsonWriter(p);
+		
+		testJson2();
+		
+		
 	}
 	
 	
@@ -138,13 +144,78 @@ public class JsonTest {
 		
 		logger.debug("person json object: \n {}", personObject);
 		
+	}
+	
+	
+	private static void testJson2(){
 		
-		
-		
-		
-		
-		
-		
+        JsonObjectBuilder payloadBuilder = Json.createObjectBuilder();
+        JsonArrayBuilder packageClaimsBuilder = Json.createArrayBuilder();
+        
+        payloadBuilder.add("requestId", 11);
+        payloadBuilder.add("requestedPackageName", "SGM portal");
+        payloadBuilder.add("justification", "request access");
+        payloadBuilder.add("requestorName", "Tom Johnson");
+        payloadBuilder.add("requestorIdentityNumber", "310826198004283713");
+        payloadBuilder.add("requestorPhone", "18301588382");
+        payloadBuilder.add("requestorType", "Supplier");
+        payloadBuilder.add("requestorTitle", "PortalSupplierUser");
+        payloadBuilder.add("requestorCompanyName", "SupplierA");
+        payloadBuilder.add("subject", "Supplier access request.");
+        payloadBuilder.add("sgmPurchaserUID", "aaa");
+        payloadBuilder.add("sgmPurchaserEmail", "bai.dong@covisint.com");
+        
+        List<String> packageClaims = new ArrayList<String>();
+        packageClaims.add("1_3_4_value_desc");
+        packageClaims.add("2_4_5_value2_desc2");
+        
+        for(String claim : packageClaims){
+            packageClaimsBuilder.add(claim);
+        }
+        
+        payloadBuilder.add("packageClaims", packageClaimsBuilder);
+        
+        String payload = payloadBuilder.build().toString();
+        
+        System.out.println(payload);
+	}
+	
+	
+	private static void testJson(){
+		JsonObjectBuilder payloadBuilder = Json.createObjectBuilder();
+		   
+		   payloadBuilder.add("firstName", "Superuser (S-FEBIDM-FIDM4)");
+		   payloadBuilder.add("middleName", "test");
+		   payloadBuilder.add("lastName", "Covisint");
+		   
+		   payloadBuilder.add("identityNo", "310826198005273963");
+		   
+		   payloadBuilder.add("email", "bai.dong@covisint.com");
+		   payloadBuilder.add("phone", "18501655384");
+		   payloadBuilder.add("mobilePhone", "18501655384");
+		   
+		   payloadBuilder.add("address", "HongQiao Rd");
+		   payloadBuilder.add("postalCode", "48075");
+		   
+		   payloadBuilder.add("jobTitle", "developer");
+		   payloadBuilder.add("language", "en");
+		   
+		   payloadBuilder.add("company", "S-FEBIDM-FIDM4");
+		   payloadBuilder.add("duns", "12345");
+		   payloadBuilder.add("state", "");
+		   payloadBuilder.add("country", "US");
+		   
+		   payloadBuilder.add("serviceIds", "sgmService1, sgmService2");
+		   
+		   payloadBuilder.add("cuid", "FHQ61E37");
+		   
+		   payloadBuilder.add("sgmUserId", "sgmTestId");
+		   
+		   String payload = payloadBuilder.build().toString();
+		   
+		   logger.debug("constructed json body: \n {}", payload);
+		   
+		   System.out.println(payload);
 	}
 	
 }
