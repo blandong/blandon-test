35a99390ded6a0fac46a0a5d3f6178491a0dbe23
bdong <bai.dong@covisint.com>
2017-06-30 11:05:45 -0400

    update

--- src/main/java/com/blandon/test/TestController.java
+++ src/main/java/com/blandon/test/TestController.java
@@ -3,12 +3,14 @@ package com.blandon.test;
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blandon.test.bean.User;
 
-@RestController
+@RestController //tells Spring to render the resulting string directly back to the caller.
+@EnableAutoConfiguration //This annotation tells Spring Boot to “guess” how you will want to configure Spring, based on the jar dependencies that you have added. Since spring-boot-starter-web added Tomcat and Spring MVC, the auto-configuration will assume that you are developing a web application and setup Spring accordingly.
 public class TestController {
 	
 	@RequestMapping("/")
