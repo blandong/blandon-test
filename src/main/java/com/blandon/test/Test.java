bef9b62506e98a8581106ca58c8292b712bbcd41
bdong <bai.dong@covisint.com>
2018-03-08 16:34:43 -0500

    xstream

--- src/main/java/com/blandon/test/Test.java
+++ src/main/java/com/blandon/test/Test.java
@@ -1,7 +1,32 @@
 package com.blandon.test;
 
+import com.blandon.test.bean.User;
+import com.thoughtworks.xstream.XStream;
+
 public class Test {
 	public static void main(String[] args) {
+		userToXml();
+		xmlToUser();
+	}
+	
+	public static void userToXml() {
+		XStream xstream = new XStream();
+		User user = new User("Blandon", 30);
+		//This is an optional step. Without it XStream would work fine, but the XML element names would contain the fully qualified name of each class (including package) which would bulk up the XML a bit
+		xstream.alias("user", User.class); 
+		String xml = xstream.toXML(user);
+		System.out.println("user in xml:\n"+xml);
+	}
+	
+	public static void xmlToUser() {
+		XStream xstream = new XStream();
+		User user = new User("Blandon", 30);
+		//This is an optional step. Without it XStream would work fine, but the XML element names would contain the fully qualified name of each class (including package) which would bulk up the XML a bit
+		xstream.alias("user", User.class); 
+		String xml = xstream.toXML(user);
+		
+		User newUser = (User)xstream.fromXML(xml);
+		System.out.println(String.format("new user nam:%s, new user age: %d}", newUser.getName(), newUser.getAge()));
 		
 	}
 }
--- src/main/java/com/blandon/test/bean/User.java
+++ src/main/java/com/blandon/test/bean/User.java
@@ -3,6 +3,16 @@ package com.blandon.test.bean;
 public class User {
 	private String name;
 	private int age;
+	
+	
+	public User() {
+		super();
+	}
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
 	/**
 	 * @return the name
 	 */
