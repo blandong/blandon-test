b59fa851926ecd3478540a16e70620f31fd2fe12
bdong <bdong@opentext.com>
2020-01-28 11:38:31 -0500

    init

--- src/main/java/com/blandon/test/Test.java
+++ src/main/java/com/blandon/test/Test.java
@@ -1,7 +1,37 @@
 package com.blandon.test;
 
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import com.blandon.test.bean.User;
+import com.google.common.flogger.FluentLogger;
+import com.google.common.flogger.LazyArgs;
+
 public class Test {
+	
+	private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+	
 	public static void main(String[] args) {
-		
+		 	logger.atWarning().log("test warning log");
+
+	        logger.atInfo().log("test info log");
+
+	        logger.at(Level.SEVERE)
+	                .atMostEvery(50, TimeUnit.SECONDS)
+	                .log("SEVERE", LazyArgs.lazy(() -> doSomeCostly()));
+	        
+	        //logger.atWarning().withCause().log("warning");
+	        
+	        User user=new User();
+	        logger.atInfo().log("Info logs : %s", user);
+
+	    }
+
+
+
+
+	public static int doSomeCostly() {
+	    return 0;
 	}
+	
 }
