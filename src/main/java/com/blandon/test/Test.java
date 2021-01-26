5c87ffa119587b7cc831e08c07517220edebaa55
bdong <bdong@opentext.com>
2021-01-26 16:12:13 -0500

    java reflection

--- src/main/java/com/blandon/test/Test.java
+++ src/main/java/com/blandon/test/Test.java
@@ -1,7 +1,27 @@
 package com.blandon.test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.blandon.test.bean.User;
+
 public class Test {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		
+		//Get the printName method and set its accessible to true.
+		Method method = User.class.getDeclaredMethod("printName", String.class);
+		method.setAccessible(true);
+		
+		//invoke the printName(String anything) to print passed parameter - myReflectionTest
+		User user = new User();
+		method.invoke(user, "myReflectionTest");
+		
+		//set name with reflection since name setter method does not exist, then call getName method.
+		ReflectionTestUtils.setField(user, "name", "myName");
+		System.out.println(user.getName());
+		
 		
 	}
 }
--- src/main/java/com/blandon/test/bean/User.java
+++ src/main/java/com/blandon/test/bean/User.java
@@ -9,24 +9,10 @@ public class User {
 	public String getName() {
 		return name;
 	}
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	/**
-	 * @return the age
-	 */
-	public int getAge() {
-		return age;
-	}
-	/**
-	 * @param age the age to set
-	 */
-	public void setAge(int age) {
-		this.age = age;
-	}
 	
+	private String printName(String anything) {
+		System.out.println(anything);
+		return anything;
+	}
 	
 }
