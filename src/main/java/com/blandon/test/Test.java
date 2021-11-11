aaf9fa950477e90421f5d74a20c03d47ab853741
bdong <bdong@opentext.com>
2021-11-11 17:03:15 -0500

    selenium test

--- src/main/java/com/blandon/test/Test.java
+++ src/main/java/com/blandon/test/Test.java
@@ -1,7 +1,29 @@
 package com.blandon.test;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Test {
 	public static void main(String[] args) {
+		// declaration and instantiation of objects/variables  
+	    System.setProperty("webdriver.chrome.driver", "/Users/bdong/tmp/selenium/chromedriver");  
+	    
+	    WebDriver driver=new ChromeDriver();  
+	      
+	    // Launch website  
+	    driver.navigate().to("http://www.google.com/");  
+	          
+	    // Click on the search text box and send value  
+	    driver.findElement(By.name("q")).sendKeys("javatpoint tutorials");  
+	          
+	    // Click on the search button  
+	    List<WebElement>  searchButton = driver.findElements(By.name("btnK"));
+	    
+	    searchButton.get(1).click();
 		
 	}
 }
